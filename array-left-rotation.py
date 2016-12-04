cc24a0bca957b9b025ff88fce534b51ce3ef1322
Golembieski <BillGolembieski@projectu23.com>
2016-12-04 17:33:06 -0500

    Array Left Rotation Challenge 12/04/2016

--- array-left-rotation.py
+++ array-left-rotation.py
@@ -38,8 +38,8 @@ arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
 
 
 def left_shift(array, num_left_shifts):
-    # Input: Un-shifted array, number of left shifts to perform 
-    # Output: None. Global array is changed. 
+    """ Input: Un-shifted array, number of left shifts to perform
+        Output: None. Global array is changed. """
     for step in range(num_left_shifts):
         temp = int(array[0])
         array.append(temp)
@@ -47,8 +47,8 @@ def left_shift(array, num_left_shifts):
     
     
 def format_answer(answer_array):
-    # Input: Array with correct index values for solution
-    # Output: String that is correctly formatted for the challenge
+    """ Input: Array with correct index values for solution
+        Output: String that is correctly formatted for the challenge."""
     ans_string = ""  
     
     for num in arr:
